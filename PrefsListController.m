9eff9089d9d05503e0dc8ee802dbcebde8ef0703
MTAC <mtactheming@gmail.com>
2020-10-11 13:00:21 -0400

    Update PrefsListController.m

--- PrefsListController.m
+++ PrefsListController.m
@@ -9,7 +9,7 @@
 //
 //                                       //
 	
-- (void)openController:(LynxButton *)sender {
+- (void)openController:(TweakButton *)sender {
 	AudioServicesPlaySystemSound(1519); // Add haptic feedback if you want 
 	NSString *className = sender.identifier;
 	HBRootListController *controller = [[NSClassFromString(className) alloc] init]; // Use the base class of your sub prefs controller here. PSListController iirc
