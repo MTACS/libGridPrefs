7629172f598901673d9402b2e4fd58c57127b494
MTAC <mtactheming@gmail.com>
2020-10-11 12:55:16 -0400

    Update PrefsListController.h

--- PrefsListController.h
+++ PrefsListController.h
@@ -1,1 +1,11 @@
+@protocol TweakTableView
+- (id)initWithSpecifier:(PSSpecifier *)specifier;
+- (CGFloat)preferredHeightForWidth:(CGFloat)width;
+@end
 
+@interface TweakCustomCell: PSTableCell <TweakTableView>
+@end
+
+@interface TweakButton: UIButton
+@property (nonatomic, retain) NSString *identifier;
+@end
