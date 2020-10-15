8a21edc940a212f5598fd9d1825b38ef7dbfbb13
SouthernGirlWhoCode <70700757+SouthernGirlWhoCode@users.noreply.github.com>
2020-10-15 16:19:05 +0100

    Update PrefsListController.h
    Removed interface and property

--- PrefsListController.h
+++ PrefsListController.h
@@ -4,14 +4,5 @@
 #import <AudioToolbox/AudioServices.h> // If you want haptic feedback on buttons
 #import <Preferences/PSTableCell.h>
 
-@protocol TweakTableView
-- (id)initWithSpecifier:(PSSpecifier *)specifier;
-- (CGFloat)preferredHeightForWidth:(CGFloat)width;
-@end
-
-@interface TweakCustomCell: PSTableCell <TweakTableView>
-@end
-
-@interface TweakButton: UIButton
-@property (nonatomic, retain) NSString *identifier;
-@end
+@interface PrefsListController : PSListController
+@end 
